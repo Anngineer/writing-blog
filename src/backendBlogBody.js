a6fc145735458c80130e0f75d76430fbbf52f035
Ann <anngineer@gmail.com>
2022-04-26 16:58:04 -0400

    Changes names of blog examples

--- src/backendBlogBody.js
+++ src/backendBlogBody.js
@@ -2,8 +2,8 @@ const backendBlogBody = [
   {
     body: (
       <p>
-        1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. <br />
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. <br />
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
         ut aliquip ex ea commodo consequat.{" "}
       </p>
@@ -15,8 +15,8 @@ const backendBlogBody = [
   {
     body: (
       <p>
-        2 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+        ut aliquip ex ea commodo consequat. Duis aute irure dolor in
         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
         pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
         qui officia deserunt mollit anim id est laborum.
@@ -28,8 +28,8 @@ const backendBlogBody = [
   {
     body: (
       <p>
-        3 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+        ut aliquip ex ea commodo consequat. Duis aute irure dolor in
         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
         pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
         qui officia deserunt mollit anim id est laborum.
@@ -42,9 +42,8 @@ const backendBlogBody = [
   {
     body: (
       <p>
-        {" "}
-        4 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+        ut aliquip ex ea commodo consequat. Duis aute irure dolor in
         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
         pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
         qui officia deserunt mollit anim id est laborum.
--- src/backendCards.js
+++ src/backendCards.js
@@ -4,21 +4,21 @@ import WatercolorPoppy1 from "./images/WatercolorPoppy1.png";
 import TwistedSessionFlyerTemplate from "./images/TwistedSessionFlyerTemplate.jpg";
 const backendCards = [
   {
-    title: "First Post",
+    title: "Cyberpunk Pandas",
     author: "Author Name",
     alt: "retro tech image with a pink play button",
     image: TwistedSessionFlyerTemplate,
-    blurb: "1 Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+    blurb: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
     tags: ["sci fi"],
     key: 1,
     id: 1,
   },
 
   {
-    title: "Second Post",
+    title: "Oleander and Rye",
     author: "Author Name",
     alt: "a girl with dark hair presses a hand to her mouth",
-    blurb: "2 Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+    blurb: "Duis aute irure dolor in reprehenderit...",
 
     image: retrogirl6a,
     tags: ["fantasy"],
@@ -27,10 +27,10 @@ const backendCards = [
     id: 2,
   },
   {
-    title: "Third",
+    title: "Serendipity",
     author: "Author Name",
     alt: "graphic image of astronauts in front of the moon",
-    blurb: "3 Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+    blurb: "Excepteur sint occaecat cupidatat non proident...",
 
     image: spacetravel1,
     tags: ["sci fi"],
@@ -40,11 +40,11 @@ const backendCards = [
   },
 
   {
-    title: "Fourth Post",
+    title: "Overcast Reflections",
     author: "Author Name",
     alt: "watercolor poppy flowers",
     blurb:
-      "4 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat...",
+      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat...",
 
     image: WatercolorPoppy1,
     tags: ["nonfiction"],
